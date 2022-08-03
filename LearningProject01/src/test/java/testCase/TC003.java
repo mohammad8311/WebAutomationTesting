c8ec1acf891c9e4d3414c9345f9d83e291a44ff1
mohammad8311 <mohammad15-8311@diu.edu.bd>
2022-08-03 18:07:38 +0600

    Updated

--- LearningProject01/src/test/java/testCase/TC003.java
+++ LearningProject01/src/test/java/testCase/TC003.java
@@ -1,7 +1,42 @@
 package testCase;
 
+import java.time.Duration;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import base.DriverSetup;
 
 //Verify the page title
-public class TC003 {
+public class TC003 extends DriverSetup {
+
+	public static String baseUrl = "http://automationpractice.com/index.php";
+	
+	
+	@Test(priority=1)
+	public void verifyPageTitle_ShoudBePass()
+	{
+		driver.get(baseUrl);
+		new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		
+		String GetTitleFromWebsite = driver.getTitle();
+		System.out.println("Website Title is: "+GetTitleFromWebsite);
+	}
+	
+	
+	@Test(priority=2)
+	public static void WebsiteSecurityStatus()
+	{
+		String x = driver.getCurrentUrl();
+		if(x.contains("https"))
+		{
+			System.out.println("This website is secured");
+		}
+		else
+		{
+			System.out.println("This website is not secured");
+		}
+	}
 
 }
